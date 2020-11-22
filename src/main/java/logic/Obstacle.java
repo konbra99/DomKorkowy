13f55999e56e5fb9283f8f946c5d204b198848aa
damianbalinski <damianbalinski99@gmail.com>
2020-11-22 21:35:20 +0100

    Klasa Obstacle

--- src/main/java/logic/Obstacle.java
+++ src/main/java/logic/Obstacle.java
@@ -7,7 +7,7 @@ public class Obstacle extends Entity {
 	public Obstacle(float posX, float posY, float width, float height, String textureName) {
 		this.rectangle = new Rectangle(posX, posY, width, height);
 		this.textureName = textureName;
-		this.rectangle.initGL(this.textureName);
+		this.rectangle.initGL(this.textureName, "rectangle.vert.glsl", "rectangle.frag");
 		this.groups |= GROUP_OBSTACLES;
 	}
 

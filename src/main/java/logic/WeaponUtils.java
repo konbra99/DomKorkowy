febb7edb5f84d6d72199eea2f7b0f5e2e366ff31
damianbalinski <damianbalinski99@gmail.com>
2020-11-24 15:41:05 +0100

    Klasa WeaponUtils, testy to WeaponUtils.

--- src/main/java/logic/WeaponUtils.java
+++ src/main/java/logic/WeaponUtils.java
@@ -3,47 +3,51 @@ package logic;
 
 public class WeaponUtils {
 
-	// kierunek uderzenia
-	public static int CLOCKWISE = 1;
-	public static int NONCLOCKWISE = -1;
-
 	// funkcja uderzenia
 	public final static int FUNC1 = 0;
 	public final static int FUNC2 = 1;
 	public final static int FUNC3 = 2;
+
+	// kierunek uderzenia
+	public static int CLOCKWISE = 1;
+	public static int NONCLOCKWISE = -1;
+
+	// stale matematyczne
+	public final static float DEG_TO_RAD = 0.01745329251f;      /*  2pi/360  */
+	public final static float RAD_TO_DEG = 57.2957795131f;      /*  360/2pi  */
 
 	/**
 	 * Oblicza kat odchylenia miecza w radianach. Wszystkie funkcje sa znormalizowan
 	 * do [0,1]x[0,1], manipulujac parametrami zwiekszamy ten zakres
 	 * @param function   wybrana funkcja uderzenia
 	 * @param direction  kierunek uderzenia        (zgodnie/przeciwnie do ruchu wskazowek zegara)
-	 * @param angle      maksymalny kat odchylenie (w radianach)
+	 * @param angle      maksymalny kat odchylenie (w stopniach)
 	 * @param time       czas trwania uderzenia    (w klatkach)
 	 * @param frame      bierzaca klatka
 	 * @return kat odchylenia
 	 */
-	public static float angle(int function, int direction, int angle, int time, int frame) {
+	public static float angle(int function, int direction, float angle, int time, int frame) {
 		float x = (float)frame / (float)time;
-		float y = 0.0f;
 
-		switch(function) {
-			case FUNC1:
-				y = func1(x);
-			case FUNC2:
-				y = func2(x);
-			case FUNC3:
-				y = func3(x);
-		}
+		float y = switch (function) {
+			case FUNC1 -> func1(x);
+			case FUNC2 -> func2(x);
+			case FUNC3 -> func3(x);
+			default -> 0.0f;
+		};
 
-		return direction * angle * y;
+		return DEG_TO_RAD * angle * direction * y;
 	}
 
 	/**
 	 * Zwraca tablice wszystkich katow odchylen.
 	 * @return tablica katow odchylen
 	 */
-	public static float[] angleTable() {
-		return null;
+	public static float[] anglesArray(int function, int direction, float angle, int time) {
+		float [] angles = new float[time];
+		for(int i = 0; i < time; i++)
+			angles[i] = angle(function, direction, angle, time, i);
+		return angles;
 	}
 
 	// parabola wklesla
---
+++ src/test/java/logic/WeaponUtilsTest.java
@@ -0,0 +1,28 @@
+package logic;
+
+import org.junit.Test;
+
+import java.time.temporal.WeekFields;
+
+public class WeaponUtilsTest {
+
+	//@Test
+	public void angleTest() {
+		float angle = 30f;
+		int time = 40;
+		for(int i = 0; i < time; i++)
+			System.out.println(WeaponUtils.angle(
+					WeaponUtils.FUNC3,
+					WeaponUtils.CLOCKWISE,
+					angle, time, i));
+	}
+
+	//@Test
+	public void anglesArrayTest() {
+		float angle = 30f;
+		int time = 40;
+		float [] angles = WeaponUtils.anglesArray(WeaponUtils.FUNC1, WeaponUtils.CLOCKWISE, angle, time);
+		for(var a: angles)
+			System.out.println(a);
+	}
+}

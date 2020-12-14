3969a97a2ba02e6173249d6408532dd847ba54d3
konbra99 <konbra99@wp.pl>
2020-12-14 21:18:15 +0100

    nic

--- src/main/java/graphics/gui/EditorContext.java
+++ src/main/java/graphics/gui/EditorContext.java
@@ -1,6 +1,32 @@
 package graphics.gui;
 
+import graphics.Engine;
+
+class ElementButton extends Button {
+    EditorContext editor;
+
+    public ElementButton(EditorContext.CHOSEN state, String[] textures, EditorContext editor) {
+        super(0.0f, 0.0f, 0.3f, 0.1f, null, textures);
+        this.editor = editor;
+        this.action = () -> {
+            editor.state = state;
+        };
+    }
+}
+
 public class EditorContext extends Context {
+    enum CHOSEN {
+        SELECT,
+        PLATFORM,
+        OBSTACLE,
+        MOB
+    }
+
+    CHOSEN state;
+
+    ElementButton newElement = null;
+
+
     public EditorContext() {
         super("background/sky.png");
         super.init();
